efa41233d76337dbb66e8b6f91625836adb5a228
eloylp <eloylpdev@gmail.com>
2018-12-28 10:26:14 +0100

    adding dependency download to build script.

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+pushd .
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$HERE/go_multi_arch_build.sh github.com/eloylp/go-telegram-uploader $HERE/build 'windows/amd64|linux/amd64|linux/arm/5'
+cd $HERE
+go get ./...
+./go_multi_arch_build.sh github.com/eloylp/go-telegram-uploader $HERE/build 'windows/amd64|linux/amd64|linux/arm/5'
+popd
